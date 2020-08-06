4efbb5500b92f5ed04f81d1154c428f738248e31
June103310110 <june103310110@gmail.com>
2020-08-06 10:23:59 +0100

    yolo_infer

--- RPI/cupoy_rpi/cupoy_rpi/flask_yolo.py
+++ RPI/cupoy_rpi/cupoy_rpi/flask_yolo.py
@@ -1,20 +1,17 @@
 # yolo  + Flask
 
 import os
-import argparse
-import imghdr
 import sys
 from flask import Flask
 import time
-sys.path.append('/home/pi/darknet/')
 
+sys.path.append('/home/pi/darknet/')
+#from new_darknet import performDetect
 app = Flask(__name__)
 
 
-def default_yolo():
+def default_yolo(): 
     path = os.getcwd()
-#    def hello():
-#        return 'hello, Here is June'
     imgpath = '/home/pi/Downloads/tug.jpg'
     os.chdir('/home/pi/darknet')
     from new_darknet import performDetect
@@ -24,21 +21,16 @@ def default_yolo():
     os.chdir(path)
     return a
 
-
-#@app.route("/")
-#def hello():
-#    a = default_yolo('/home/pi/Downloads/tug.jpg')
-#    return a 
-
-#@app.route("/")
-#def hello():
-#    text0 = 'detect: '+ans[0][0]+'\tconfidence: '\
-#        +str(format(ans[0][1], '.5f'))
-#    text2 = '\ntime is: '+time.strftime("%Y-%m-%d %H:%M:%S", time.localtime())
-#    return text0+text2
+def defult_display():
+    ans = default_yolo()
+    text0 = 'detect: '+ans[0][0]+'\tconfidence: '\
+        +str(format(ans[0][1], '.5f'))
+    text2 = '\ntime is: '+time.strftime("%Y-%m-%d %H:%M:%S", time.localtime())
+    reply = text0 + text2
+    return reply
 
 imgYolo = True
-app_go = False
+
 if __name__ == "__main__":
     
     if not imgYolo:
@@ -47,24 +39,17 @@ if __name__ == "__main__":
             return 'hello i am June'
         
         app.run(host='10.17.4.132', port = 8080,
-           debug = True, threaded = True)
+           debug = False, threaded = True)
     
 
     
     if imgYolo:
-        def defult_display():
-            ans = default_yolo()
-            text0 = 'detect: '+ans[0][0]+'\tconfidence: '\
-                +str(format(ans[0][1], '.5f'))
-            text2 = '\ntime is: '+time.strftime("%Y-%m-%d %H:%M:%S", time.localtime())
-            reply = text0 + text2
-            print(__name__)
-            return reply
-        replya = defult_display()
+
+        reply = defult_display()
         
         @app.route("/")
         def abc():
             return reply
         
         app.run(host='10.17.4.132', port = 8080,
-           debug = True, threaded = True)
+           debug = False, threaded = True)
